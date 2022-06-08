a0a8fc12edc31b035dc0b8e874262e399c36deb2
damirdine <damirdine@outlook.fr>
2022-06-08 09:45:19 +0200

    add bootstrap

---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include_once('config.php');
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MiniTchat</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">GBAF</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="d-flex">
+                <?php if (!isset($_SESSION['logged_user'])) : ?>
+                    <a href='./login.php' class="btn btn-primary me-2" type="button">connexion
+                    </a>
+                    <a href='./signup.php' class="btn btn-primary bg-light text-primary me-2" type="button">inscription
+                    </a>
+                <?php else : ?>
+                    <div class="dropdown">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?= $_SESSION['logged_user'] ?>
+                        </a>
+
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Profil</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item bg-danger" href='./logout.php'  type="button">deconexion</a></li>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </nav>
+    <main class="container">
--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php 
+include_once 'header.php';
 include 'register.php';
