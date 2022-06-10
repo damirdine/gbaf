66524f163e90c6836fac02c93c884bad05917fbc
damirdine <damirdine@outlook.fr>
2022-06-10 09:28:17 +0200

    login ✅

--- user/login.php
+++ user/login.php
@@ -1,5 +1,6 @@
 <?php include_once('header.php');
-if(isset($_POST['password']) && isset($_POST['username'])){
+
+if(isset($_POST['password'],$_POST['username'])){
     $checkUserSqlREquest = "SELECT * FROM users WHERE username = :username;";
     $checkUser = $db -> prepare($checkUserSqlREquest);
     $checkUser->execute(
@@ -7,12 +8,12 @@ if(isset($_POST['password']) && isset($_POST['username'])){
             'username'=>$_POST['username'],
         ]
     )or die(print_r($db->errorInfo()));
-    $user = $checkUser->fetch();
+    $user = $checkUser->fetch();var_dump($user);
     if($_POST['username']!==$user['username']){
         $userNotExist = 'Aucun compte lié a ce username. Veillez Créé un compte';
     }
     if(password_verify($_POST['password'],$user['password'])){
-        $userLoggeded=$user['firstname'].' '.$user['name'];
+        $userLogged=$user['firstname'].' '.$user['name'];
         $_SESSION['logged_user_name'] = $userLogged;
         $_SESSION['logged_user'] = $user['username'];
         $_SESSION['logged_user_id'] = $user['id'];
@@ -29,15 +30,15 @@ if(isset($_POST['password']) && isset($_POST['username'])){
     <?php elseif(isset($passworNotMatch)):?>
         <p class="alert alert-warning mt-4"><?php echo(htmlspecialchars($passworNotMatch))?>.</p>
     <?php endif;?>
-    <form class="container m-5 d-grid gap-4" method='POST' action='../'>
+    <form class="container m-5 d-grid gap-4" method='POST' action='./'>
         <h1>Connexion</h1>
         <div class="form-group">
             <label for="exampleInputEmail1">Mail</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="jhondoe@exemple.com">
+            <input type="text" name="username" class="form-control" aria-describedby="usernameInput" placeholder="jhondoe">
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Mot de passe</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+            <input type="password" name="password" class="form-control" placeholder="Mot de passe">
         </div>
         <div class="form-group">       
             <button type="submit" class="btn btn-primary">Connexion</button>
